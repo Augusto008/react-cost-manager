7c9b8012f8ffb813b18135d8877c7df995e83ffd
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-03-23 22:30:21 -0300

    feat: add coin icon and projects path to navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,33 @@
 import { Link } from "react-router-dom";
 import styles from "./Navbar.module.css";
+import Container from "./Container";
+import logo from "../../img/coin.png";
 
 function Navbar() {
   return (
-    <nav className='menu'>
-      <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/contact'>Contact</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/company'>Company</Link>
-        </li>
-        <li className={styles.item}>
-          <Link to='/newproject'>New Project</Link>
-        </li>
-      </ul>
+    <nav className={styles.navbar}>
+      <Container>
+        <Link to="/">
+          <img src={logo} alt="Cost Manager" className={styles.logo} />
+        </Link>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to='/Projects'>Projects</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to='/newproject'>New Project</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to='/company'>Company</Link>
+          </li>
+          <li className={styles.item}>
+            <Link to='/contact'>Contact</Link>
+          </li>
+        </ul>
+      </Container>
     </nav>
   )
 }
