37c6043b1b4867d0339a2259ea4767d1278eca41
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-03-24 07:14:22 -0300

    feat: create Home page and style

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css';
+import financial_report from '../../img/financial-report.svg'; 
+import LinkButton from '../layout/LinkButton';
+
 function Home() {
   return (
-    <h1>Home</h1>
+    <section className={styles.home_container}>
+      <h1>Welcome to <span>Cost Manager</span></h1>
+      <p>Start managing your projects!</p>
+      <LinkButton to="/NewProject" text="New Project" />
+      <img src={financial_report} alt="Cost Manager" />
+    </section>
   )
 }
 
