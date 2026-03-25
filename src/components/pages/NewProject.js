80d39bb73cf0cd6de4c3cadcb35ea5771b7a3f3f
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-03-24 23:31:32 -0300

    feat: create NewProjects page

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,6 +1,13 @@
+import ProjectForm from "../project/ProjectForm";
+import styles from './NewProject.module.css';
+
 function NewProject() {
   return (
-    <h1>NewProject</h1>
+    <div className={styles.newproject_container}>
+      <h1>NewProject</h1>
+      <p>Create your project to add services.</p>
+      <ProjectForm />
+    </div>
   )
 }
 
