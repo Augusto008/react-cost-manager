0758544310d5d952e57350eb5c6b36bc35881c16
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-03-25 21:51:52 -0300

    feat: create SubmitButton component

---
+++ src/components/form/SubmitButton.js
@@ -0,0 +1,11 @@
+import styles from './SubmitButton.module.css';
+
+function SubmitButton({ btnText }) {
+  return (
+    <div>
+      <button className={styles.btn}>{btnText}</button>
+    </div>
+  )
+}
+
+export default SubmitButton;
