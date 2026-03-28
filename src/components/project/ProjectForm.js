ad3cbf1a746a0f1d74df88e443d0b4cc27b4b93e
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-03-28 18:56:47 -0300

    feat (ProjectForm.js): fetch categories from API

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,8 +2,25 @@ import Input from '../form/Input';
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css';
+import { useEffect, useState } from 'react';
 
 function ProjectForm({ btnText }) {
+
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => setCategories(data))
+      .catch(error => console.error('Error fetching categories:', error));
+  }, []);
+
+
   return (
     <form className={styles.form}>
       <Input 
@@ -18,7 +35,8 @@ function ProjectForm({ btnText }) {
         placeholder='Project Budget' />
       <Select 
         name='category_id'
-        text='Select the category' />
+        text='Select the category'
+        options={categories} />
       <SubmitButton 
         text={btnText} />
     </form>
