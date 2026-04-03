5960a4593d0c34d8e4a7e376ff285f84e5265e71
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-04-03 19:28:45 -0300

    refactor (Projects): remove unnecessary console.log

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -27,7 +27,6 @@ function Projects() {
       },
     }).then((resp) => resp.json())
       .then((data) => {
-        console.log(data);
         setProjects(data);
         setRemoveLoading(true);
       })
