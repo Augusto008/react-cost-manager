6b6d4e5c80d6e36c41db10ac1db58a85cb542e0c
Augusto008 <49340003+Augusto008@users.noreply.github.com>
2026-04-04 16:59:13 -0300

    feat (Project): create removeService function and list service cards

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -6,6 +6,7 @@ import { useState, useEffect } from 'react';
 import Container from '../layout/Container';
 import ProjectForm from '../project/ProjectForm';
 import ServiceForm from '../service/ServiceForm';
+import ServiceCard from '../service/ServiceCard';
 import Message from '../layout/Message';
 
 function Project() {
@@ -13,6 +14,7 @@ function Project() {
   const { id } = useParams();
 
   const [project, setProject] = useState([]);
+  const [services, setServices] = useState([]);
   const [showProjectForm, setShowProjectForm] = useState(false);
   const [showServiceForm, setShowServiceForm] = useState(false);
   const [message, setMessage] = useState();
@@ -28,6 +30,7 @@ function Project() {
     .then(resp => resp.json())
     .then(data => {
       setProject(data);
+      setServices(data.services);
     })
     .catch(error => {
       console.error('Error fetching project:', error);
@@ -106,6 +109,39 @@ function Project() {
 
   }
 
+  function removeService(id, cost) {
+
+    setMessage('');
+    
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    );
+
+    const projectUpdated = project;
+
+    projectUpdated.services = servicesUpdated;
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(projectUpdated)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setProject(projectUpdated);
+      setServices(servicesUpdated);
+      setMessage('Service removed successfully!');
+      setType('success');
+    })
+    .catch(error => {
+      console.error('Error removing service:', error);
+    });
+
+  }
+
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
   }
@@ -162,7 +198,18 @@ function Project() {
             </div>
             <h2>Services</h2>
             <Container customClass="start">
-              <p>List of services will go here</p>
+              {services.length > 0 && 
+                services.map((service) => (
+                  <ServiceCard 
+                    id={service.id}
+                    name={service.name}
+                    cost={service.cost}
+                    description={service.description}
+                    key={service.id}
+                    handleRemove={removeService} />
+                ))
+              }
+              {services.length === 0 && <p>No services added yet.</p>}
             </Container>
           </Container>
         </div>
